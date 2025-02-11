3c9257fbee3ff4a0e76097f1d461749f394c1207
seiyeon <wsx4379@naver.com>
2025-02-11 09:46:32 +0900

    [Init] 프로젝트 생성
    [Init] 프로젝트 생성

---
+++ src/main/java/com/example/idus_exam/IdusExamApplication.java
@@ -0,0 +1,13 @@
+package com.example.idus_exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdusExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdusExamApplication.class, args);
+	}
+
+}
