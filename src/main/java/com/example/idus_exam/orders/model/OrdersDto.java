b07e813e24e71215364f98543294b1f84f8f243c
seiyeon <wsx4379@naver.com>
2025-02-11 16:16:15 +0900

    [Fix] Orders 엔티티에 orderId 필드 누락 추가
    Orders 엔티티에 orderId(주문번호) 누락 추가

--- src/main/java/com/example/idus_exam/orders/model/OrdersDto.java
+++ src/main/java/com/example/idus_exam/orders/model/OrdersDto.java
@@ -12,11 +12,14 @@ import java.time.LocalDateTime;
 public class OrdersDto {
     @Getter
     public static class OrderRegister {
+        @Schema(description = "주문번호", example = "order01")
+        private String orderId;
         @Schema(description = "제품명", example = "product01")
         private String name;
 
         public Orders toEntity(User user) {
             return Orders.builder()
+                    .orderId(orderId)
                     .name(name)
                     .user(user)
                     .build();
@@ -28,8 +31,10 @@ public class OrdersDto {
     @Getter
     @Builder
     public static class OrdersResponse {
-        @Schema(description = "주문번호", example = "1")
+        @Schema(description = "idx", example = "1")
         private Long idx;
+        @Schema(description = "주문번호", example = "order01")
+        private String orderId;
         @Schema(description = "제품명", example = "product01")
         private String name;
         @Schema(description = "결제일시", example = "2025-02-11T15:24:09.286965")
@@ -38,6 +43,7 @@ public class OrdersDto {
         public static OrdersResponse from(Orders orders) {
             return OrdersResponse.builder()
                     .idx(orders.getIdx())
+                    .orderId(orders.getOrderId())
                     .name(orders.getName())
                     .date(orders.getDate())
                     .build();
